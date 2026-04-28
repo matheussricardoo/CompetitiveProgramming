08be86b028e9c442c691654ef48cbde6f2094784
matheussricardoo <matheusri1@hotmail.com>
2026-04-27 21:34:07 -0300

    Removing the warning for the “moves” variable in problem 263A in Rust

--- Codeforces/Rust/800-1000/263A - Beautiful Matrix/src/main.rs
+++ Codeforces/Rust/800-1000/263A - Beautiful Matrix/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    let mut moves = 0;
+    let mut _moves = 0;
 
     for i in 1..=5i32 {
         input.clear();
@@ -11,8 +11,8 @@ fn main() {
         for val_str in input.split_whitespace() {
             let v: i32 = val_str.parse().expect("It's not a number");
             if v == 1 {
-                moves = (i - 3).abs() + (j as i32 - 3).abs();
-                println!("{}", moves);
+                _moves = (i - 3).abs() + (j as i32 - 3).abs();
+                println!("{}", _moves);
                 return;
             }
             j += 1;
